8e525d35214315d054024694a15fbe7ee6c64066
Jamel Toms <jameltoms@gmail.com>
2019-04-06 12:49:26 -0400

    Added link to context-menu component

--- src/views/demos/context-menu.vue
+++ src/views/demos/context-menu.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Context Menu</h1>
+  <div class="context-menu">
+    <h1>Context Menu</h1>
+    <context-menu></context-menu>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ContextMenu from '@/components/context-menu/context-menu.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    ContextMenu
+  }
+})
+export default class extends Vue {}
+</script>
